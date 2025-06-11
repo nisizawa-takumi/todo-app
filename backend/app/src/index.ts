4b29153fe5727e106bac9922607f1a9c10f270c9
Sota Ura <sota1202@A090367119.rfg.rfgad.ricoh.com>
2025-06-11 17:12:51 +0900

    API作成

--- backend/app/src/index.ts
+++ backend/app/src/index.ts
@@ -1,12 +1,64 @@
+import { PrismaClient } from '@prisma/client';
 import express from 'express';
 
 const app = express();
 const port = process.env.PORT || 4000;
 
+const prisma = new PrismaClient();
+
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello, Express + TypeScript!');
+app.get('/allTodos', async (req, res) => {
+    try {
+        const allTodos = await prisma.todo.findMany();
+        res.json(allTodos);
+    } catch (error) {
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+app.post("/createTodo", async (req, res) => {
+    try {
+        const { title, isCompleted } = req.body;
+        const createTodo = await prisma.todo.create({
+            data: {
+                title,
+                isCompleted,
+            },
+        });
+        res.json(createTodo);
+    } catch (e) {
+        res.status(400).json(e);
+    }
+});
+
+app.put("/editTodo/:id", async (req, res) => {
+    try {
+        const id = Number(req.params.id);
+        const { title, isCompleted } = req.body;
+        const editTodo = await prisma.todo.update({
+            where: { id },
+            data: {
+                title,
+                isCompleted,
+            },
+        });
+        res.json(editTodo);
+    } catch (e) {
+        res.status(400).json(e);
+    }
+});
+
+app.delete("/deleteTodo/:id", async (req, res) => {
+    try {
+        const id = Number(req.params.id);
+        const deleteTodo = await prisma.todo.delete({
+            where: { id },
+        });
+        res.json(deleteTodo);
+    } catch (e) {
+        res.status(400).json(e);
+    }
 });
 
 app.listen(port, () => {
---
+++ backend/app/src/sample.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, test } from '@jest/globals';
+
+describe('サンプルテスト', () => {
+    test('1 + 1 は 2 になる', () => {
+        expect(1 + 1).toBe(2);
+    });
+});
